6023545b8139c7850a95cec3e79f8fcc40e1d1ab
bulivif <valchev@live.com>
2017-06-13 23:25:13 +0300

    the second example is added

--- ex1.js
+++ ex1.js
@@ -11,3 +11,22 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+// write a recursive function 
+
+function isEven(n){
+    if (n == 0)
+    return true;
+    else if (n == 1)
+    return false;
+    else 
+    return isEven(n - 2);
+}
+
+console.log(isEven(50));
+// -> true
+
+console.log(isEven(75));
+// -> false
+console.log(isEven(-1));
+// -> false
