82364a370684342cbadc75ee5105a68f6965faa9
bulivif <valchev@live.com>
2017-06-14 22:32:10 +0300

    another example

--- ex1.js
+++ ex1.js
@@ -30,3 +30,23 @@ console.log(isEven(75));
 // -> false
 console.log(isEven(-1));
 // -> false
+
+// bean counting function
+
+function countChar(string, ch){
+  var counted = 0;
+  for(var i = 0; i < string.length; i++)
+  if (string.charAt(i) == ch)
+  counted +=1;
+  return counted;
+}
+
+function countBs(string){
+  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// -> 2
+console.log(countChar("kakkerlak", "k"));
+
+// -> 4
